88e0e92d95f6b3dc475b281edd4f10794ead93ac
nahidsultan19 <nahidsultan38@gmail.com>
2022-03-29 23:01:30 +0600

    coin detail completed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Coins from './components/Coins/Coins';
 import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Homepage from './components/Homepage/Homepage';
+import About from './components/About/About';
+import CoinDetails from './components/CoinDetails/CoinDetails';
 
 const App = () => {
   return (
@@ -12,8 +15,11 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/coins' element={<Coins />} />
+        <Route path='/coin-details/:id' element={<CoinDetails />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='/about' element={<About />} />
       </Routes>
+      <Footer></Footer>
     </div>
   )
 }
---
+++ src/components/CoinDetails/CoinDetails.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const CoinDetails = () => {
+    const { id } = useParams()
+    const [coin, setCoin] = useState({})
+    useEffect(() => {
+        const url = `https://api.coingecko.com/api/v3/coins/${id}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setCoin(data)
+            })
+    }, [id])
+    return (
+        <div className='px-4 h-{70vh} pt-20 pb-24 mx-auto max-w-7xl md:px-2'>
+            <div className='h-full grid grid-cols-1 md:grid-cols-2 justify-items-center gap-4 md:justify-items-around content-center'>
+                <div className=''>
+                    <h1 className='text-3xl'>General Info: </h1>
+                    <hr />
+                    <h1>Coin name: {coin.name}</h1>
+                    <h1>Market Cap: {coin.market_cap_rank}</h1>
+                    <h1>Origin: {coin.country_origin ? coin.country_origin : 'Not Found'}</h1>
+
+                    <h1 className='text-3xl'>Scores: </h1>
+                    <hr />
+                    <h1>Community Score : {coin.community_score}</h1>
+                    <h1>Developer Score : {coin.developer_score}</h1>
+                </div>
+                <div className='flex justify-center items-center'>
+                    <img src={coin.image?.large} alt="coinImg" />
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default CoinDetails;
